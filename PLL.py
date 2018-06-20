e1179b2813d837c6b3af5ff0aaa8f320d6d43759
David A. Dahlbom <david.dahlbom@gmail.com>
2018-06-20 17:43:31 -0400

    removed pll as function. now only available as an object.

--- PLL.py
+++ PLL.py
@@ -53,42 +53,6 @@ class PLL:
         self.out = out[self.buf_size-1:]
         self.freqs = freq_offset+f_c 
 
-def pll_func(in_sig, f_c, f_s):
-    '''
-    Implementation of a digital phase-locked loop (PLL) as presented in
-    Sethares' "Rhythm and Transforms" book (2007).  Uses a gradient-ascent
-    approach, correlating the input signal with an internal oscillator,
-    adjusting the phase of the internal oscillator to maximize the correlation.
-    The frequency estimate is found by calculating the gradient of the phase.
-    '''
-    mu = 0.120*(f_c/4000.)
-    dt = 1./f_s
-    lp_cutoff = f_c/5.0
-    b_lp, a_lp = dsp.bessel(2, (lp_cutoff*2)/f_s)
-    b_s, a_s = dsp.bessel(2, (50.0*2)/f_s)      # smoothing filter
-    buf_size = max(len(b_lp), len(a_lp))
-    in_sig = np.concatenate( (np.zeros(buf_size-1), in_sig)) 
-    phi = np.zeros_like(in_sig)
-    out = np.zeros_like(in_sig)
-    mod_sig = np.zeros(buf_size, dtype=np.float32)
-    lp_out = np.zeros(buf_size, dtype=np.float32)
-    idx0 = 2
-    idx1 = 1
-    idx2 = 0
-    for k in range(buf_size - 1, len(in_sig)):
-        t = (k - buf_size + 1)*dt
-        idx0 = (idx0 + 1)%buf_size
-        idx1 = (idx1 + 1)%buf_size
-        idx2 = (idx2 + 1)%buf_size
-        mod_sig[idx0] = in_sig[k]*np.sin(2*np.pi*f_c*t + phi[k-1]) 
-        lp_out[idx0] = b_lp[0]*mod_sig[idx0] + b_lp[1]*mod_sig[idx1] \
-                     + b_lp[2]*mod_sig[idx2] - a_lp[1]*lp_out[idx1] \
-                     - a_lp[2]*lp_out[idx2]
-        phi[k] = phi[k-1] - mu*lp_out[idx0]
-        out[k] = np.cos(2*np.pi*f_c*t + phi[k])
-    freq_offset = np.gradient(phi[buf_size-1:], dt)/(2*np.pi)
-    freq_offset = dsp.filtfilt(b_s, a_s, freq_offset)   #smooth freq estimates
-    return out[buf_size-1:], (freq_offset+f_c)
 
 ##############################################################################
 if __name__ == "__main__":
