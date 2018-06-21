6a5fec14b5efba132a01bec69314118b690d781a
David A. Dahlbom <david.dahlbom@gmail.com>
2018-06-21 14:07:05 -0400

    Returned PLL to function status (since f_c must be recalculated every time applied in any case, there is no savings involved in allocating in advance). Move the PLL function, pll, to a file together with FDL class.  Called this file scfbutils.py

--- SCFB.py
+++ SCFB.py
@@ -1,8 +1,7 @@
 import numpy as np
 import scipy.signal as dsp
 import matplotlib.pyplot as plt
-from FDL import FDL
-from PLL import PLL
+from scfbutils import FDL, pll
 
 class SCFB:
     '''
@@ -15,14 +14,46 @@ class SCFB:
     implemented in a separated class), when the FDLs report a locked
     condition.
     '''
-    def __init__(self, f_lo, f_hi, num_chan, f_s):
-        pass
+    def __init__(self, f_lo, f_hi, num_chan, f_s, verbose=False):
+        # basic parameters and placeholders
+        self.f_s = f_s
+        self.dt = 1./f_s
+        self.num_chan = num_chan
+        self.chunks = []
+
+        # calculate frequencies and bandwidths of channels
+        self.f_c = np.logspace(np.log10(f_lo), np.log10(f_hi), num_chan)
+        c = 2.**(1./6.) - 1/(2.**(1./6.))   # bw multiplier
+        self.bw = [ max(100.0, f_c*c) for f_c in self.f_c ] 
+
+        # Set up filter coefficients for each channel
+        self.a = []
+        self.b = []
+        for k in range(self.num_chan):
+            b, a = dsp.butter(2, np.array([max(self.f_c[k] - self.bw[k],
+                self.f_c[0]), self.f_c[k] + self.bw[k]])*(2/f_s),
+                btype='bandpass')
+            self.a.append(a)
+            self.b.append(b)
+
+        # Set up FDLs and PLLs for each channel
+        self.fdl = [FDL(self.f_c[k], self.bw[k], self.f_s) for k in
+                        range(self.num_chan)]
+        # self.pll = [PLL(self.f_c[k], self.f_s) for k in range(self.num_chan)]
+
 
     def process_signal(self, in_sig):
-        pass
+        for k in range(self.num_chan):
+            filted = dsp.filtfilt(self.b[k], self.a[k], in_sig)
+            f0s, idx_chunks, out_chunks = self.fdl[k].process_data(filted)
+
+
+
+
 
     def plot_output(self, in_sig):
         pass
+
 
     def get_ordered_output(self, in_sig):
         pass
@@ -30,4 +61,4 @@ class SCFB:
 
 ################################################################################
 if __name__=="__main__":
-    print("Still nothing!")
+    scfb = SCFB(80, 4000, 50, 44100, verbose=True)
